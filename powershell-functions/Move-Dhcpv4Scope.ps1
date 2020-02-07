ae0510b0ecb39c3a13d25f6fc9806e72492ef51b
Ryan Micahel Patterson <ryanmichaelpatterson@icloud.com>
2020-02-07 15:36:29 -0800

    Merge of Move-Dhcpv4Scope.ps1
    Updated Move-Dhcpv4Scope.ps1 scope to include comment based help.

--- powershell-functions/Move-Dhcpv4Scope.ps1
+++ powershell-functions/Move-Dhcpv4Scope.ps1
@@ -1,49 +1,89 @@
 ﻿function Move-DhcpServerv4Scope{
+    <#
+    .SYNOPSIS
+    The Move-DhcpServerv4Scope moves a dhcp scope from a target server to a destenation server. 
+        
+    .DESCRIPTION
+    The Move-DhcpServerv4Scope cmdlet moves a dhcp scope, its reservations, its exclusions, its options, 
+    and its leases from a target server to destentaion server. The dhcp reservations, exclusions, options, 
+    and leases are be excluded from the move by specifing a false value in their associated parameter. 
+        
+    .PARAMETER SourceServer
+    Specifies the source dhcp server that the target scope resides on. 
+
+    .PARAMETER DestenationServer
+    Specifies the destenation server that the target scope will be moved to.
+
+    .PARAMETER DhcpScopeId
+    Specifes the dhcp scopeid that will be moved.
+
+    .PARAMETER DhcpReservations
+    Specifes if the cmdlet will move dhcp reservations from the target scope. 
+
+    .PARAMETER DhcpExclusions
+    Specifes if the cmdlet will move dhcp exclusions from the target scope. 
+
+    .PARAMETER DhcpOptions
+    Specifes if the cmdlet will move dhcp options from the target scope. 
+
+    .PARAMETER DhcpLeases
+    Specifes if the cmdlet will move dhcp leases from the target scope. 
+
+    .OUTPUTS
+
+        
+    .EXAMPLE
+    Migrate the dhcp scope with scopeId 192.168.0.1 from sfo1-dhcp01 to sfo1-dhcp02.
+    C:\PS> Move-DhcpServerv4Scope -SourceServer sfo1-dhcp01 -DestenationServer sfo1-dhcp02 -DhcpScopeId 192.168.0.1
+    
+    .FUNCTIONALITY
+    Windows DHCP
+    #>
 
     [CmdletBinding()]
     Param(
 
         [Parameter(Mandatory=$true, HelpMessage = "Specify the hostname of the source dhcp server")]
         [ValidateScript({Test-Connection -ComputerName $_ -Count 1})]
-        [string] $sourceServer,
+        [string] $SourceServer,
 
         [Parameter(Mandatory=$true, HelpMessage = "Specify the hostname of the destenation dhcp server")]
         [ValidateScript({Test-Connection -ComputerName $_ -Count 1})]
-        [string] $destenationServer,
+        [string] $DestenationServer,
 
         [Parameter(Mandatory=$true, HelpMessage = "Specify the ScopeId of the target dhcp scope.")] 
-        [IPAddress] $dhcpScopeId,
+        [IPAddress] $DhcpScopeId,
 
         [Parameter(HelpMessage = "Specify if dhcp reservations should be migrated")]
-        [bool] $dhcpReservations = $true,
+        [bool] $DhcpReservations = $true,
 
         [Parameter(HelpMessage = "Specify if dhcp exclusions should be migrated")]
-        [bool] $dhcpExclusions = $true,
+        [bool] $DhcpExclusions = $true,
 
         [Parameter(HelpMessage = "Specify if dhcp options should be migrated")]
-        [bool] $dhcpOptions =$true,
+        [bool] $DhcpOptions = $true,
 
         [Parameter(HelpMessage = "Specify if dhcp leases should be migrated")]
-        [bool] $dhcpLeases = $true
+        [bool] $DhcpLeases = $true
     )
 
     Begin{
 
-        #Invoke Get-ADComputer on sourceServer parameter. 
+        #Invoke Get-ADComputer on sourceServer parameter to set varible to ADComputer object. 
         try{
-            Write-Verbose -Message "Invoking Get-ADComputer cmdlet on hostname: $sourceServer"
-            $sourceADComputerObject = Get-ADComputer -Identity $sourceServer 
+            Write-Verbose -Message "Invoking Get-ADComputer cmdlet on hostname: $SourceServer"
+            $sourceADComputerObject = Get-ADComputer -Identity $SourceServer
         }catch{
-            throw "Unable to invoke Get-ADComputer cmdlet on hostname: $sourceServer"
+            throw "Unable to invoke Get-ADComputer cmdlet on hostname: $SourceServer"
             break
         }
 
-        #Invoke Get-ADComputer on destenationServer parameter. 
+        #Invoke Get-ADComputer on destenationServer parameter to set varible to ADComputer object. 
         try{
-            Write-Verbose -Message "Invoking Get-ADComputer cmdlet on hostname: $destenationServer"
-            $destenationADComputerObject = Get-ADComputer -Identity $destenationServer   
+            Write-Verbose -Message "Invoking Get-ADComputer cmdlet on hostname: $DestenationServer"
+            $destenationADComputerObject = Get-ADComputer -Identity $DestenationServer   
         }catch{
-            throw "Unable to invoke Get-ADComputer cmdlet on hostname: $destenationServer"
+            throw "Unable to invoke Get-ADComputer cmdlet on hostname: $DestenationServer"
             break
         }
     }
@@ -52,7 +92,7 @@
 
         #Attempt to migrate the DHCP scope from the source server to the destination server. 
         try { 
-            Write-Verbose -Message "Invoking Get-DhcpServerv4Scope on scope: $dhcpScopeId from server: $($sourceADComputerObject.Name)"
+            Write-Verbose -Message "Invoking Get-DhcpServerv4Scope on scope: $DhcpScopeId from server: $($sourceADComputerObject.Name)"
             $tempScope = Get-DhcpServerv4Scope -ComputerName $sourceADComputerObject.Name -ScopeId $dhcpScopeId 
             try{
                 Write-Verbose -Message "Invoking Add-DhcpServerv4Scope object on $destenationServer"
@@ -67,9 +107,9 @@
         }    
 
         #Migrate DHCP Reservations from source server to destination server. 
-        if ($dhcpReservations){ 
+        if ($DhcpReservations){ 
             try{
-                Write-Verbose -Message "Invoking Get-Dhcpv4Reservation on scope: $dhcpScopeId from server: $($sourceADComputerObject.Name)"
+                Write-Verbose -Message "Invoking Get-Dhcpv4Reservation on scope: $DhcpScopeId from server: $($sourceADComputerObject.Name)"
                 $tempReservations = Get-DhcpServerv4Reservation -computername $sourceADComputerObject.Name -ScopeId $dhcpScopeId 
                 
                 if ($tempReservations){
@@ -88,9 +128,9 @@
         }
 
         #Migrate DHCP Exclusions from source server to destination server. 
-        if ($dhcpExclusions){
+        if ($DhcpExclusions){
             try{
-                Write-Verbose -Message "Invoking Get-DhcpServerv4ExclusionRange on scope: $dhcpScopeId on server: $($sourceADComputerObject.Name)"
+                Write-Verbose -Message "Invoking Get-DhcpServerv4ExclusionRange on scope: $DhcpScopeId on server: $($sourceADComputerObject.Name)"
                 $tempExclusions = Get-DhcpServerv4ExclusionRange -ComputerName $sourceADComputerObject.Name -ScopeId $dhcpScopeId 
                 
                 if($tempExclusions){
@@ -109,9 +149,9 @@
         }
 
         #Migrate DHCP Options from source server to destination server. 
-        if ($dhcpOptions){  
+        if ($DhcpOptions){  
             try{
-                Write-Verbose -Message "Invoking Get-DhcpServerv4OptionValue on scope: $dhcpScopeId from server: $($sourceADComputerObject.Name)"
+                Write-Verbose -Message "Invoking Get-DhcpServerv4OptionValue on scope: $DhcpScopeId from server: $($sourceADComputerObject.Name)"
                 $tempOptions = Get-DhcpServerv4OptionValue -ComputerName $sourceADComputerObject.Name -ScopeId $dhcpScopeId 
                 
                 if($tempOptions){
@@ -130,9 +170,9 @@
         }
 
         #Migrate DHCP Leases from source server to destination server. 
-        if ($dhcpLeases){
+        if ($DhcpLeases){
             try{
-                Write-Verbose -Message "Invoking Get-DhcpServerv4Lease on scope: $dhcpScopeId from server: $($sourceADComputerObject.Name)"
+                Write-Verbose -Message "Invoking Get-DhcpServerv4Lease on scope: $DhcpScopeId from server: $($sourceADComputerObject.Name)"
                 $tempLeases = Get-DhcpServerv4Lease -ComputerName $sourceADComputerObject.Name -ScopeId $dhcpScopeId 
                 
                 if($tempLeases){
